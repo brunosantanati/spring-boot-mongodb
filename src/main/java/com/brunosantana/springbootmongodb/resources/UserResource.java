41a904aa3b1c283d5452cc8be7cedfe87da133d9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-12 14:53:18 -0300

    Implemented delete user using http DELETE method

--- src/main/java/com/brunosantana/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/brunosantana/springbootmongodb/resources/UserResource.java
@@ -45,4 +45,10 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/brunosantana/springbootmongodb/services/UserService.java
+++ src/main/java/com/brunosantana/springbootmongodb/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repo.insert(user);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
