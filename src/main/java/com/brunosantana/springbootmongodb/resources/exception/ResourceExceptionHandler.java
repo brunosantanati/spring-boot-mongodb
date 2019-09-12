02a2700b60b1af880fbd09e96fa6ab45fc6eb8ee
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-12 11:31:43 -0300

    Implemented the functionality to get user by id and exception handler

---
+++ src/main/java/com/brunosantana/springbootmongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.brunosantana.springbootmongodb.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.brunosantana.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+	
+}
--- src/main/java/com/brunosantana/springbootmongodb/services/UserService.java
+++ src/main/java/com/brunosantana/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.brunosantana.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunosantana.springbootmongodb.domain.User;
 import com.brunosantana.springbootmongodb.repository.UserRepository;
+import com.brunosantana.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> optional = repo.findById(id);
+		return optional.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
